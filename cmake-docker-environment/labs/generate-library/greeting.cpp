371f09bca90eb4a1fe11344afa338523259a94b6
NickR2600 <43765727+NickR2600@users.noreply.github.com>
2022-04-21 18:21:07 -0400

    Add files via upload

--- cmake-docker-environment/labs/generate-library/greeting.cpp
+++ cmake-docker-environment/labs/generate-library/greeting.cpp
@@ -1,9 +1,6 @@
 #include "greeting.h"
 #include <iostream>
-#include <string>
-
-std::string greetingString = "Hello World!";
 
 void sayHello() {
-	std::cout << greetingString +"\n";
+	std::cout << "Hello World!\n";
 }
